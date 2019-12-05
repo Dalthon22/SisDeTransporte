93edfade8114a174cc04c8f97e26132b92799058
Dalton Diaz <cdaltondiaz@gmail.com>
2019-12-05 03:40:14 -0600

    Login funcionando rico
    Usuarios con email, contraseña y esten activos

--- transporte/routes/index.js
+++ transporte/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const Employee = require('../models/m_employee');
 const db = require('../dbconfig/conex');
 
 
@@ -11,13 +12,14 @@ router.post('/auth', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
     if (username && password) {
-        db.query('SELECT * FROM SGT_Usuario WHERE email = ? AND password = ?', {
-                replacements: [username, password],
+        db.query('SELECT * FROM SGT_Usuario u inner join SGT_Empleado e on e.user_id = u.id WHERE u.email = ? AND u.password = ? AND u.active = ?', {
+                replacements: [username, password, true],
                 type: db.QueryTypes.SELECT
             }).then(results => {
                 console.log(results);
                 if (results.length > 0) {
-                    var usuario = results[0].NombresUsuario;
+                    console.log(results)
+                    var usuario = results[0].first_name + ' ' + results[0].last_name;
                     console.log(usuario);
                     var url = encodeURI('/home?usuario=' + usuario)
                     res.redirect(url);
